b8ed6b5cd9515eb91e0ed19200b90461d2347ceb
Nicaredda <workcaredda1998@gmail.com>
2025-12-15 15:29:13 +0100

    added week button generator; connected colleagues list with local storage

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # The MAB ARCHEOTUR work shifts assigner
 
 An automated work shifts assigner, specifically designed for mab archeotur
+
+
+**TO DO**
+1) Implementare la possibilità di cambiare l'assegnazione di ogni collega in lista ad un turno della week
+il programma se arriva a quel collega in monday morning, salterebbe il collega andando ad inserire il collega nell'apposito spazio
+2) portare week generata in tabella html
--- script.js
+++ script.js
@@ -9,13 +9,13 @@ console.log("this is a test")
 // con la logica richiesta ogni giorno e ogni turno. 
 // ************************************************************************************************************************************************************
 // TO DO: 
-//      1) Inserire un elemento input che permetta di cambiare i giorni liberi e di ferie dei colleghi in lista; 
-//         utilizzeremo Local Storage del browser per salvare queste assegnazioni chiave/valore (perchè non capisco come fare tramite file json locale...lol)
-//      2) Implementare la possibilità di cambiare l'assegnazione di ogni collega in lista ad un turno della week, in base ad una specifica richiesta, e.g. Collga[0] requestedDay: monday.evening. 
-//         il programma se arriva a quel collega in monday morning, salterebbe il collega andando ad inserire il collega nell'apposito spazio
-
-
-
+// 1) DONE Inserire un elemento input che permetta di cambiare i giorni liberi e di ferie dei colleghi in lista; 
+//      utilizzeremo Local Storage del browser per salvare queste assegnazioni chiave/valore (perchè non capisco come fare tramite file json locale...lol)
+// 2) Implementare la possibilità di cambiare l'assegnazione di ogni collega in lista ad un turno della week, in base ad una specifica richiesta, e.g. Collga[0] requestedDay: monday.evening. 
+//      il programma se arriva a quel collega in monday morning, salterebbe il collega andando ad inserire il collega nell'apposito spazio
+// 3) DONE modificare dinamicamente la lista oggetti colleghi con i valori in localstorage, prendendo come dati validi solamente i dati che corrispondono al nome di giorni della settimana
+// 4) transporre la settimana generata come una tabella in html 
+// 5) DONE realizzare un pulsante in html per eseguire le generazioni randomiche della week
 
 const colleaguesList = [
     {name: "michela", workDays: 6, freeDay:"saturday", vacationDays: [], workedDays: 0},
@@ -29,8 +29,6 @@ const colleaguesList = [
     {name: "ritaP", workDays: 6, freeDay: "wednesday", vacationDays: [], workedDays: 0},
     {name: "rita" ,workDays: 6, freeDay:  "sunday", vacationDays: [], workedDays: 0}
 ]
-
-
 
 const week = [
     monday = { d: "monday",
@@ -97,169 +95,14 @@ const week = [
         morningNecropoli2:undefined, eveningMuseo3:undefined
     }
 ]
-
+// Ci prendiamo subito i valori in localstorage con la funzione che riassegna ad ogni elemento in lista colleagues i suoi valori salvati
+colleaguesListValuesAssignment();
 
 console.log("**NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST****NEW TEST**")
 
-// loop per settare il freeDay in local storage
-for (let i = 0; i<colleaguesList.length; i++) {
-    let name = colleaguesList[i].name
-    let freeDay = colleaguesList[i].freeDay;
-    localStorage.setItem(name, freeDay)
-}
-
-let alternated;
-if (localStorage.getItem("alternated") === "false") {  // <== facciamo riferimento al valore booleano in local storage per sapere come è il valore di alternated in seguito alla precedente esecuzione del codice
-     alternated = false
-} else {
-    alternated = true
-}
-
-//************************************************************************************************* LOGICA DI ASSEGNAZIONE RANDOMICA SPECIFICA PER IL MAB ******************************************************************************************************
-for (const day in week) {
-    let workingList = [] 
-    for (const worker in colleaguesList) {
-        if (colleaguesList[worker].freeDay.includes(week[day].d)  || colleaguesList[worker].vacationDays.includes(week[day].d)) {
-           continue;
-        } workingList.push(colleaguesList[worker].name);
-    }
-
-    for (const key of Object.keys(week[day])) { 
-        if (key === "d") { //  <== Skippa una iterazione se la chiave è "d", perchè "d" è la chiave contenente il nome del giorno, e non vogliamo sovrascriverla 
-            continue;
-        } if ((key === "morningNecropoli1" || key === "eveningNecropoli1") && alternated === true) {
-            week[day][key] = "COLLEGA ETNOGRAFICO"
-            alternated = false
-            continue;
-        } else if ((key === "morningNecropoli1" || key === "eveningNecropoli1") && alternated === false) {
-            alternated = true
-        }
-        let randomIndex = randomNumber(workingList);
-        let randomColleague = workingList[randomIndex];
-        week[day][key] = randomColleague;
-    
-        if (workingList.includes(randomColleague)) {  // <== depenniamo i colleghi dalla lista man mano che li assegniamo
-            for (worker in colleaguesList) {
-                if (colleaguesList[worker].name === randomColleague) {
-                            colleaguesList[worker].workedDays ++
-                }
-            }
-            workingList.splice(randomIndex,1,)
-        };
-    }
-
-    if (alternated === false) { //<== logica di alternazione per il collega etnografico, la cui presenza è alternata ogni periodo e il sistema di alternazione si alterna ogni giorno
-        alternated = true
-    } else { 
-        alternated = false
-    }
-}  localStorage.setItem("alternated", alternated) // <== utilizziamo per convenienza il local storage del browser per cambiare il valore booleano ad alternated
+// ***********************************************PARTE DOM DI COSTRUZIONE TABELLA COLLEAGUES LIST IN HTML ******************************************************************************
 
 
-// QUI SOTTO VIENE SISTEMATA LA LISTA IN MODO CHE SIA FORMATTATA MEGLIO. I TURNI VENGONO RIMESSI IN ORDINE
-//_NECESSARIO, DATO CHE SOPRA NELLA LOGICA DEL CODICE VENGONO MESSI IN ORDINE DI PRIORITà DI ASSEGNAZIONE
-
-
-let turns = {
-    monday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    tuesday:   { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    wednesday: { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    thursday:  { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    friday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    saturday:  { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-    sunday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
-};
-const dayNames = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
-
-
-
-for (let i = 0; i < week.length; i++) {
-    const day = week[i];
-    const name = day.d;
-
-    turns[name].morning.museo = [
-        day.morningMuseo1,
-        day.morningMuseo2,
-        day.morningMuseo3,
-    ];
-
-    turns[name].morning.necropoli = [
-        day.morningNecropoli1,
-        day.morningNecropoli2,
-    ];
-
-    turns[name].evening.museo = [
-        day.eveningMuseo1,
-        day.eveningMuseo2,
-        day.eveningMuseo3,
-    ];
-
-    turns[name].evening.necropoli = [
-        day.eveningNecropoli1,
-        day.eveningNecropoli2,
-    ];
-}
-
-
-const text = document.querySelector(`body`);
-
-// E qui sotto in loop mostriamo la scheda week ordinata nel file html
-for (const dayName in turns) {
-    
-    const day = turns[dayName];
-    
-    let nomeGiorno = `======== ${dayName.toUpperCase()} ========`;
-    console.log(nomeGiorno);
-    
-    let element = document.createElement(`div`);
-    const nextLine = document.createElement(`br`)
-    element.textContent = nomeGiorno
-    text.append(element)
-    text.append(nextLine)
-    
-    let mattinaMuseo = `Mattina Museo: ${day.morning.museo.join(", ")}`;
-    console.log(mattinaMuseo);
-
-    element = document.createElement(`div`);
-    element.textContent = mattinaMuseo
-    text.append(element)
-    text.append(nextLine)
-
-    let mattinaNecropoli = `Mattina Necropoli:, ${day.morning.necropoli.join(", ")}`;
-    console.log(mattinaNecropoli);
-    
-    element = document.createElement(`div`);
-    element.textContent = mattinaNecropoli
-    text.append(element)
-    text.append(nextLine)
-    
-    let seraMuseo = `Sera Museo: ${day.evening.museo.join(", ")}`;
-    console.log(seraMuseo)
-    
-    element = document.createElement(`div`);
-    element.textContent = seraMuseo
-    text.append(element)
-    text.append(nextLine)
-    
-    let seraNecropoli = `Sera Necropoli: ${day.evening.necropoli.join(", ")}`;
-    console.log(seraNecropoli);
-
-    element = document.createElement(`div`);
-    element.textContent = seraNecropoli
-    text.append(element)
-    text.append(nextLine)
-
-    console.log("\n")
-}
-
-function randomNumber(list) { 
-    randomResult = Math.floor(Math.random()*list.length);
-    return randomResult;
-}
-
-
-
-// ******************************************************* PARTE DOM ******************************************************************************
 // il bottone ci permetterà di avviare la fase di cambio valori della tabella o di conferma
 const assignmentButton = document.getElementById("assignmentButton") 
 assignmentButton.addEventListener("click", changeModeSwitch)
@@ -269,10 +112,6 @@ const vacDayText = document.querySelectorAll(".vacDayText");
 const vacDayInput = document.querySelectorAll(".vacDayInput");
 const freeDayText = document.querySelectorAll(".freeDayText")
 const freeDayInput = document.querySelectorAll(".freeDayInput")
-
-
-
-
 
 // Appena avviato il codice, questo ci permette di costruire già una tabella di base con gli ultimi valori salvati
 for (let i=0; i<freeDayText.length;i++){
@@ -330,8 +169,6 @@ function displayVacDayInput(){
     }
 }
 
-
-
 let changeMode = false
 function changeModeSwitch(){  
     if (changeMode === false) {
@@ -343,13 +180,200 @@ function changeModeSwitch(){
     } else {
         displayFreeDayText();
         displayVacDayText();
+        colleaguesListValuesAssignment();
         changeMode = false
         assignmentButton.style.backgroundColor = "red"
         assignmentButton.textContent = "Modifica"
     }
 }
-//TO DO: 
-// 1) modificare dinamicamente la lista oggetti colleghi con i valori in localstorage, prendendo come dati validi solamente i dati che corrispondono al nome di giorni della settimana
-// 2) transporre la settimana generata come una tabella in html 
-// 3) realizzare un pulsante in html per eseguire le generazioni randomiche della week
+
+function colleaguesListValuesAssignment(){
+    for (let i = 0; i<colleaguesList.length; i++){
+        colleaguesList[i].freeDay = localStorage.getItem(`freeDayCurrentText${i}`)
+        colleaguesList[i].vacationDays = localStorage.getItem(`vacDayCurrentText${i}`)
+    }
+}
+
+console.log(colleaguesList)
+// ****************************ALTERNATED CI SERVE PER LA LOGICA DI ASSEGNAZIONE DEL COLLEGA ETNOGRAFICO**************************************************************
+let alternated;
+if (localStorage.getItem("alternated") === "false") {  // <== facciamo riferimento al valore booleano in local storage per sapere come è il valore di alternated in seguito alla precedente esecuzione del codice
+     alternated = false
+} else {
+    alternated = true
+}
+
+//************************************************** LOGICA DI ASSEGNAZIONE RANDOMICA SPECIFICA PER I TURNI DELLA WEEK ******************************************************************************************************
+function generateRandomWeek(){
+
+    let allDivs = document.querySelectorAll("div");
+    let allBrs =  document.querySelectorAll("br");
+// rimuoviamo la week generata cosi che non le stacka l'una sopra l'altra. Son solo un mucchio di div e br
+    for (let i = 0; i<allDivs.length; i++){ 
+        allDivs[i].remove()
+    }  for (let i = 0; i<allBrs.length; i++){ 
+        allBrs[i].remove()
+    } 
+// Generiamo la week. Gigantesca funzione che racchiude praticamente ogni cosa
+    for (const day in week) {
+        let workingList = [] 
+        for (const worker in colleaguesList) {
+            if (colleaguesList[worker].freeDay.includes(week[day].d)  || colleaguesList[worker].vacationDays.includes(week[day].d)) {
+            continue;
+            } workingList.push(colleaguesList[worker].name);
+        }
+
+        for (const key of Object.keys(week[day])) { 
+            if (key === "d") { //  <== Skippa una iterazione se la chiave è "d", perchè "d" è la chiave contenente il nome del giorno, e non vogliamo sovrascriverla 
+                continue;
+            } if ((key === "morningNecropoli1" || key === "eveningNecropoli1") && alternated === true) {
+                week[day][key] = "COLLEGA ETNOGRAFICO"
+                alternated = false
+                continue;
+            } else if ((key === "morningNecropoli1" || key === "eveningNecropoli1") && alternated === false) {
+                alternated = true
+            }
+            let randomIndex = randomNumber(workingList);
+            let randomColleague = workingList[randomIndex];
+            week[day][key] = randomColleague;
+        
+            if (workingList.includes(randomColleague)) {  // <== depenniamo i colleghi dalla lista man mano che li assegniamo
+                for (worker in colleaguesList) {
+                    if (colleaguesList[worker].name === randomColleague) {
+                                colleaguesList[worker].workedDays ++
+                    }
+                }
+                workingList.splice(randomIndex,1,)
+            };
+        }
+
+        if (alternated === false) { //<== logica di alternazione per il collega etnografico, la cui presenza è alternata ogni periodo e il sistema di alternazione si alterna ogni giorno
+            alternated = true
+        } else { 
+            alternated = false
+        }
+    }  localStorage.setItem("alternated", alternated) // <== utilizziamo per convenienza il local storage del browser per cambiare il valore booleano ad alternated
+//***********************************DINAMIC ASSIGNMENT TO COLLEAGUE LIST ITEMS*************************************************************************************** */
+
+// QUI SOTTO VIENE SISTEMATA LA LISTA IN MODO CHE SIA FORMATTATA MEGLIO. I TURNI VENGONO RIMESSI IN ORDINE
+//_NECESSARIO, DATO CHE SOPRA NELLA LOGICA DEL CODICE VENGONO MESSI IN ORDINE DI PRIORITà DI ASSEGNAZIONE
+
+
+    let turns = {
+        monday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        tuesday:   { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        wednesday: { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        thursday:  { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        friday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        saturday:  { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+        sunday:    { morning: { museo: [], necropoli: [] }, evening: { museo: [], necropoli: [] } },
+    };
+    const dayNames = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+
+
+    for (let i = 0; i < week.length; i++) {
+        const day = week[i];
+        const name = day.d;
+
+        turns[name].morning.museo = [
+            day.morningMuseo1,
+            day.morningMuseo2,
+            day.morningMuseo3,
+        ];
+
+        turns[name].morning.necropoli = [
+            day.morningNecropoli1,
+            day.morningNecropoli2,
+        ];
+
+        turns[name].evening.museo = [
+            day.eveningMuseo1,
+            day.eveningMuseo2,
+            day.eveningMuseo3,
+        ];
+
+        turns[name].evening.necropoli = [
+            day.eveningNecropoli1,
+            day.eveningNecropoli2,
+        ];
+    }
+
+
+    const text = document.querySelector(`body`);
+
+    // E qui sotto in loop mostriamo la scheda week ordinata nel file html
+    for (const dayName in turns) {
+        
+        const day = turns[dayName];
+        
+        let nomeGiorno = `======== ${dayName.toUpperCase()} ========`;
+        console.log(nomeGiorno);
+        
+        let element = document.createElement(`div`);
+        const nextLine = document.createElement(`br`)
+        element.textContent = nomeGiorno
+        text.append(element)
+        text.append(nextLine)
+        
+        let mattinaMuseo = `Mattina Museo: ${day.morning.museo.join(", ")}`;
+        console.log(mattinaMuseo);
+
+        element = document.createElement(`div`);
+        element.textContent = mattinaMuseo
+        text.append(element)
+        text.append(nextLine)
+
+        let mattinaNecropoli = `Mattina Necropoli:, ${day.morning.necropoli.join(", ")}`;
+        console.log(mattinaNecropoli);
+        
+        element = document.createElement(`div`);
+        element.textContent = mattinaNecropoli
+        text.append(element)
+        text.append(nextLine)
+        
+        let seraMuseo = `Sera Museo: ${day.evening.museo.join(", ")}`;
+        console.log(seraMuseo)
+        
+        element = document.createElement(`div`);
+        element.textContent = seraMuseo
+        text.append(element)
+        text.append(nextLine)
+        
+        let seraNecropoli = `Sera Necropoli: ${day.evening.necropoli.join(", ")}`;
+        console.log(seraNecropoli);
+
+        element = document.createElement(`div`);
+        element.textContent = seraNecropoli
+        text.append(element)
+        text.append(nextLine)
+
+        console.log("\n")
+    }
+
+}
+
+
+function randomNumber(list) { 
+    randomResult = Math.floor(Math.random()*list.length);
+    return randomResult;
+}
+
+
+console.log(colleaguesList)
+
+
+
+const generateWeekButton = document.getElementById("generateWeek")
+generateWeekButton.addEventListener("click", generateRandomWeek);
+
+
+
+
+
+
+
+
+
+
 
